9955b7177a450063207168fe640d14ff700589fe
opinaka <m.attik@opinaka.com>
2023-11-22 11:43:45 +0100

    hh

--- projects/BlocklyML-python/static/js/blocks_ML.js
+++ projects/BlocklyML-python/static/js/blocks_ML.js
@@ -20,6 +20,30 @@
     Blockly.Constants.Colour.HUE = 20;
     Blockly.defineBlocksWithJsonArray([
         {
+            "type": "between",//AND-BLOCK
+            "message0": "    %1",//Text on Block (First Input) --> messages need to be numbereds
+            "args0": [
+                {
+                    "type": "input_value",//FirstInput
+                    "name": "Con0",//name of FirstInput
+                    "check": ['CONDITIONCHOOSER', 'freeinput']//Acceptance Condition for following Blocks to be combined
+                }
+            ],
+            "message1": "BETWEEN %1",//Text on second Input
+            "args1": [
+                {
+                    "type": "input_value",//SecondInput
+                    "name": "Con1",//name of SecondInput
+                    "check": ['AND']//Acceptance Condition for following Blocks to be combined
+                }
+            ],
+            "output": "BETWEEN",//Condition of this Block defining, how this Block could be combined to previous Blocks
+            "colour": '#5270DE',
+            "helpUrl": "",
+        },
+    ]);
+    Blockly.defineBlocksWithJsonArray([
+        {
             type: "colour_picker",
             message0: "%1",
             args0: [{ type: "field_colour", name: "COLOUR", colour: "#ff0000" }],
